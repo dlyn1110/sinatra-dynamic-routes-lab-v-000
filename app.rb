530b13f573695a406e939c6cc1bb0be25a3d7920
Dani Lyn <dlyn1110@gmail.com>
2018-07-26 18:20:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,4 +21,17 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
+
+
+    case params[:operation]
+    when 'add'
+      (params[:number1].to_i + params[:number2].to_i).to_s
+    when 'subtract'
+      (params[:number1].to_i - params[:number2].to_i).to_s
+    when 'multiply'
+      (params[:number1].to_i * params[:number2].to_i).to_s
+    when 'divide'
+      (params[:number1].to_i / params[:number2].to_i).to_s
+    end
+  end
 end

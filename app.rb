521300a3da451ac383721a793bbe5cf981a29124
Dani Lyn <dlyn1110@gmail.com>
2018-07-26 18:20:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-
+  get '/:operation/:number1/:number2' do
     case params[:operation]
     when 'add'
       (params[:number1].to_i + params[:number2].to_i).to_s
